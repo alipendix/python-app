e81a0fc395eb7f1978930501ffa835549e5c7805
Adrian Llera <adrian.llera.rodriguez@gmail.com>
2026-08-05 13:36:37 +0200

    feat: Add python-app

---
+++ src/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details', methods=['GET'])
+def get_details():
+    return jsonify({
+        'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        'hostname': socket.gethostname()
+    })
+
+@app.route('/api/v1/healthz', methods=['GET'])
+def get_health():
+    return jsonify({
+        'status': 'up'
+    }), 200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
