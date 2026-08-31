8c53966984e1d7f2039c929d29f97f7914941320
Adrian Llera Rodriguez <adrian.llera-rodriguez@capgemini.com>
2026-08-31 15:20:28 +0200

    ci: install

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def get_details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'hostname': socket.gethostname(),
-        'message': 'Version dinamica v5!!!'
+        'message': 'Version dinamica v6!!!'
     })
 
 @app.route('/api/v1/healthz', methods=['GET'])
