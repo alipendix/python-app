ac3de2b3e8fd5613b2ae80072ed7f6ab0967ab19
Adrian Llera Rodriguez <adrian.llera-rodriguez@capgemini.com>
2026-09-01 08:36:29 +0200

    ci: permisos de escritura

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def get_details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'hostname': socket.gethostname(),
-        'message': 'Version dinamica v8!!!'
+        'message': 'Version dinamica v9!!!'
     })
 
 @app.route('/api/v1/healthz', methods=['GET'])
