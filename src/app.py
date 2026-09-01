72b7a16a588959f7b41f4ce8fff7cd739a83fd31
Adrian Llera Rodriguez <adrian.llera-rodriguez@capgemini.com>
2026-09-01 08:52:05 +0200

    feat: new metodo

--- src/app.py
+++ src/app.py
@@ -4,12 +4,13 @@ import socket
 
 app = Flask(__name__)
 
-@app.route('/api/v1/details', methods=['GET'])
-def get_details():
+@app.route('/api/v1/info', methods=['GET'])
+def get_info():
     return jsonify({
         'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'hostname': socket.gethostname(),
-        'message': 'Version dinamica v10!!!'
+        'message': 'Version dinamica v10!!!',
+        'deployed_on': 'kubernetes'
     })
 
 @app.route('/api/v1/healthz', methods=['GET'])
